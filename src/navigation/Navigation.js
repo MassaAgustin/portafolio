699f6a0af5fb2ab61256c1eb5b7dbe3f694bba4e
AgusMassa <agus.massa@hotmail.com>
2021-03-04 11:49:49 -0300

    test example

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import { Contact } from '../pages/contact'
+
+export const Navigation = () => {
+
+    return (
+        <Fragment>
+            <Contact />
+        </Fragment>
+    )
+}
---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+
+export const contact = () => {
+    return (
+        <Fragment>
+        <header>
+            <h1>Te amo pauli uwu</h1>
+        </header>
+        <section>
+            
+        </section>
+        <footer>
+            
+            
+        </footer>
+        </Fragment>
+    )
+}
