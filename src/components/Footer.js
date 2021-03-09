cd0e524f295bd86968191ed63538b61582e237b5
AgusMassa <agus.massa@hotmail.com>
2021-03-08 23:54:54 -0300

    add href to email and icon email footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import { Container, Row, Col, Image } from 'react-bootstrap'
 import linkedIn from '../assets/icons/linkedin.svg'
 import instagram from '../assets/icons/instagram.svg'
 import twitter from '../assets/icons/twitter.svg'
+import outlook from '../assets/icons/outlook.svg'
 
 import './Footer.css'
 
@@ -12,26 +13,32 @@ export const Footer = () => {
 
 
     const urlLinkedin = "https://www.linkedin.com/in/agustin-isaac-vera-massa-939548201/";
-    const urltwitter = "https://twitter.com/AgusMassa14";
-    const urlinstagram = "https://www.instagram.com/agus.massa1/";
+    const urlTwitter = "https://twitter.com/AgusMassa14";
+    const urlInstagram = "https://www.instagram.com/agus.massa1/";
+    const urlOutlook = "mailto:agus.massa@hotmail.com?subject=Interview&body=Hello Agustin Massa,";
 
     return (
         <Container fluid className="fixed-bottom mb-0 border-top justify-content-center bg-dark">
             <footer>
-                <Row className="justify-content-center p-1" xs={4}>
+                <Row className="justify-content-center ml-5 p-1" xs={4}>
                     <Col>
                         <a href={urlLinkedin} target="_blank" rel="noopener">
                             <Image className="img-footer" fluid src={linkedIn} rounded />
                         </a>
                     </Col>
                     <Col>
-                    <a href={urltwitter} target="_blank" rel="noopener">
+                    <a href={urlTwitter} target="_blank" rel="noopener">
                             <Image className="img-footer" fluid src={twitter} rounded />
                         </a>
                     </Col>
                     <Col>
-                    <a href={urlinstagram} target="_blank" rel="noopener">
+                    <a href={urlInstagram} target="_blank" rel="noopener">
                             <Image className="img-footer" fluid src={instagram} rounded />
+                        </a>
+                    </Col>
+                    <Col>
+                    <a href={urlOutlook} target="_blank" rel="noopener">
+                            <Image className="img-footer" fluid src={outlook} rounded />
                         </a>
                     </Col>
                 </Row>
